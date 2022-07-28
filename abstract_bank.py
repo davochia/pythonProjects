c53ba3b7ca5b2c121226f18981f204daf76ee495
King David <47652874+davochia@users.noreply.github.com>
2022-07-28 21:16:51 +0300

    abstract bank

---
+++ abstract_bank.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+# Class Bank
+class Bank(ABC):
+    def basicinfo(self):
+        print("This is a generic bank")
+        return "Generic bank: 0"
+
+    @abstractmethod
+    def withdraw(self):
+        pass
+
+
+class Swiss(Bank):
+
+    def __init__(self):
+        self.bal = 1000
+
+    def basicinfo(self):
+        print("This is the Swiss Bank")
+        return "Swiss Bank: " + str(self.bal)
+
+    def withdraw(self, amount):
+        if (amount > self.bal):
+            print("Insufficient funds")
+            return self.bal
+        else:
+            self.bal -= amount
+            print("Withdrawn amount: " + str(amount))
+            print("New Balance: " + str(self.bal))
+            return self.bal
+
+
+def main():
+    assert issubclass(Bank, ABC), "Bank must derive from class ABC"
+    s = Swiss()
+    print(s.basicinfo())
+    s.withdraw(30)
+    s.withdraw(1000)
+
+
+if __name__ == "__main__":
+    main()
